387583797df4ba3bde72629b0f862268a45002c8
Noah Platton <noahplatton@gmail.com>
2022-03-02 19:03:56 +0000

    Rewrite login fct to be testable

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,22 +1,55 @@
 require("dotenv").config();
 
-const bcrypt = require("bcryptjs");
+const bcryptjs = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
-import User from "../models/User";
+const User = require("../models/User");
 
+// For the time being assume user logs in with username
 async function login(req, res) {
   try {
+    // Find user based on username/password
+    const user = await User.findByUsername(req.body.username);
+    if (!user) {
+      throw new Error("No user with given username");
+    }
+    // We need to verify password
+    const userPswrd = await user.getPassword();
+    const authed = bcryptjs.compare(req.body.password, userPswrd);
+    // Once verified log user in with JWT
+    if (!!authed) {
+      const payload = { username: user.username };
+      // Sign token payload using secret from .env
+      const token = jwt.sign(payload, process.env.SECRET, { expiresIn: 1000 });
+      if (!token) {
+        throw new Error("Error in token generation");
+      }
+      res.status(200).json({
+        success: true,
+        token: "Bearer " + token,
+      });
+    } else {
+      throw new Error("User could not be authenticated");
+    }
   } catch (err) {
-    res.status(500).json({ err });
+    res.status(401);
   }
 }
 
 async function register(req, res) {
   try {
+    // Encrypt new user's password
+    const salt = await bcryptjs.genSalt();
+    const hash = await bcryptjs.hash(req.body.password, salt);
+    // Add user to the database
+    await User.create({ ...req.body, password: hash });
+    res.status(201).json({ msg: "User created!" });
   } catch (err) {
-    res.status(500).json({ err });
+    res.status(500);
   }
 }
 
-export default { login, register };
+module.exports = {
+  login,
+  register,
+};
