612356b3d42d0ef31fd325b3bb73d098e04ce310
Noah Platton <noahplatton@gmail.com>
2022-03-02 19:05:03 +0000

    null

--- client/src/tests/SwipeElement.spec.js
+++ client/src/tests/SwipeElement.spec.js
@@ -10,8 +10,8 @@ describe("SwipeElement", () => {
   test("it swipes to the right", () => {
     const swiper = screen.getByRole("region", { id: "outer-container" });
     // const swiper = screen.getByTestId("swipe-elt");
-    userEvent.mouseDown(swiper, { clientX: 400 });
-    userEvent.mouseUp(swiper, { clientX: 350 });
+    fireEvent.mouseDown(swiper, { clientX: 400 });
+    fireEvent.mouseUp(swiper, { clientX: 350 });
     expect(swiper).toBe(1);
   });
 });
