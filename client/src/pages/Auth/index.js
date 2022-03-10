bd1d276985e44e15a2d00d0dd26326a7bf09ab32
Noah Platton <noahplatton@gmail.com>
2022-03-10 16:00:45 +0000

    Update auth page

--- client/src/pages/Auth/index.js
+++ client/src/pages/Auth/index.js
@@ -1,18 +1,22 @@
-import React, { useState } from "react";
+import React from "react";
 
 import { SwipeElement } from "../../components";
+import { useAuthContext } from "../../contexts/Auth";
 
 import "./style.css";
 
-export default ({ token, setToken }) => {
-  if (!token) {
-    return (
-      <>
+export default () => {
+  const { user } = useAuthContext();
+
+  return (
+    <>
+      {user ? (
+        <h1>Welcome to the Arkives, {user}!</h1>
+      ) : (
         <div className="auth-page">
-          <SwipeElement setToken={setToken} />
+          <SwipeElement />
         </div>
-      </>
-    );
-  }
-  return <>{/* <h1>Welcome to Noah's Ark</h1> */}</>;
+      )}
+    </>
+  );
 };
