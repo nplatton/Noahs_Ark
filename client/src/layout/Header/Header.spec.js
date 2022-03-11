8a20d1f941ec0d3b4a0dcc2bf5ad04990b430726
Noah Platton <noahplatton@gmail.com>
2022-03-11 18:21:06 +0000

    Add Header test file

--- client/src/layout/Header/Header.spec.js
+++ client/src/layout/Header/Header.spec.js
@@ -1,13 +1,32 @@
 import { screen } from "@testing-library/react";
 
-import Header from "../layout";
+import { default as Header } from ".";
 
 describe("Header", () => {
+  afterEach(() => {
+    localStorage.clear();
+  });
+
   test("it renders only pub routes with no user", () => {
     renderWithProviders(<Header />);
     const links = screen.getAllByRole("link");
-    expect(links).toHaveLength(3);
-    expect(screen.getByText(/Login/)).toBeInTheDocument();
-    expect(screen.getByText(/Logout/)).not.toBeInTheDocument();
+    expect(links).toHaveLength(4);
+    expect(screen.getByText(/login/i)).toBeInTheDocument();
+    expect(screen.queryByText(/logout/i)).not.toBeInTheDocument();
+    expect(screen.queryByText(/secret/i)).not.toBeInTheDocument();
+  });
+
+  // The below test is more of an integration test
+  // Ideally a unit test would involve mocking out useAuthContext
+
+  test("renders secret page when user is logged in", () => {
+    const testToken =
+      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Im5vYWgiLCJpYXQiOjE2NDY5OTU1NzAsImV4cCI6MTY0Njk5NjU3MH0.LoZ_RS5xtGsy5jaAgNFphm5dsvgvBA5HNAJ6XOlbUCo";
+    localStorage.setItem("token", testToken);
+    renderWithProviders(<Header />);
+    const links = screen.getAllByRole("link");
+    expect(screen.queryByText(/login/i)).not.toBeInTheDocument();
+    expect(screen.getByText(/logout/i)).toBeInTheDocument();
+    expect(screen.getByText(/secret/i)).toBeInTheDocument();
   });
 });
